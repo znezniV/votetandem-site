ffa989f5d7b89e2fa764e5704cc523d951a99ae6
Daniel <daniel.e.holler@gmail.com>
2019-05-30 16:31:13 +0200

    En, De, It 50%

--- src/components/local.js
+++ src/components/local.js
@@ -5,10 +5,12 @@ let strings = new LocalizedStrings({
         headerIntro: "A platform that enables politically excluded people in Switzerand to vote — Simple to use, social, and meant to persist.",
         heroTitle: "Including All People in Politics",
         heroText: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
-        
+
         roleGiver: "The Vote Giver",
         roleRecipient: "The Vote Recipient",
-        
+
+        actionSignUp: "Give or Get a Vote Here",
+
         sectionHeaderConcept: "How it works",
         sectionHeaderBrowser: "Prepare your Browser",
         sectionHeaderFAQ: "FAQ",
@@ -27,7 +29,59 @@ let strings = new LocalizedStrings({
         footerContribute: "Do you want to contribute?"
     },
     de: {
-    }
+      headerIntro: "Eine Plattform, die es politisch ausgeschlossenen Menschen in der Schweiz ermöglicht, zu wählen - Einfach zu bedienen, sozial und dauerhaft.",
+      heroTitle: "Alle Menschen in Politik einbeziehen",
+      heroText: "In der Schweiz sind viele Personen von der Teilnahme an Abstimmungen ausgeschlossen, die meisten davon sind Ausländer mit einem Anteil von 25 % der Bevölkerung. Votetandem.org ermöglicht eine direkte Stimmabgabe durch Treffen mit Schweizerinnen und Schweizern, die ihren Wahlzettel für den Austausch zur Verfügung stellen. Die Plattform basiert auf Blockchain-Technologie für einen sicheren und unabhängigen Betrieb.",
+
+      roleGiver: "Stimmgebende Person",
+      roleRecipient: "Stimmsuchende Person",
+
+      actionSignUp: "Gib oder erhalte eine Stimme hier",
+
+      sectionHeaderConcept: "So funktioniert's",
+      sectionHeaderBrowser: "Browser vorbereiten",
+      sectionHeaderFAQ: "FAQ",
+
+      blockHeaderGiveVote: "1a. Stimme anbieten",
+      blockHeaderReceiveVote: "1b. Stimme aussuchen",
+      blockHeaderMeeting: "2. Treffen",
+      blockHeaderConformation: "3. Bestätigung",
+
+      blockDescGiveVote: "Die stimmgebende Person registriert ihre Stimme und gibt Ort, Uhrzeit und Datum auf der Plattform an, um sie verfügbar zu machen.",
+      blockDescReceiveVote: "Die stimmsuchende Person wählt eine der aufgeführten Stimmen auf der Plattform aus und fixiert somit das Treffen.",
+      blockDescMeeting: "Beide treffen sich am vereinbarten Ort, diskutieren, und die stimmberechtigte Person füllt schließlich den Stimmzettel aus.",
+      blockDescConfirmation: "Beide bestätigen nach dem Treffen, auf der Plattform, dass der Stimmzettel ausgetauscht wurde.",
+      blockDescBrowser: "Installiere die Browsererweiterung MetaMask. Schreibe deine Seedphrase auf. Wechsle zum Ropsten-Netzwerk. Hol dir Testgeld auf https://faucet.metamask.io/ und warte ein paar Minuten.",
+
+      footerContribute: "Willst du deinen Teil beitragen?"
+    },
+    it: {
+      headerIntro: "Una piattaforma che permette a persone politicamente escluse in Svizzera di partecipare al voto - Semplice da usare, sociale e destinato a persistere.",
+      heroTitle: "Includendo tutte le persone in politica",
+      heroText: "In Svizzera numerosi abitanti sono esclusi dalle elezioni, la maggior parte dei quali sono stranieri, costituendo il 25 % della popolazione. Votetandem.org offre la possibilità di votare direttamente incontrando persone svizzere che forniscono la loro scheda di voto per lo scambio. La piattaforma si basa sulla tecnologia Blockchain per un servizio sicuro e indipendente.",
+
+      roleGiver: "The Vote Giver",
+      roleRecipient: "The Vote Recipient",
+
+      actionSignUp: "Give or Get a Vote Here",
+
+      sectionHeaderConcept: "How it works",
+      sectionHeaderBrowser: "Prepare your Browser",
+      sectionHeaderFAQ: "FAQ",
+
+      blockHeaderGiveVote: "1a. Register a Vote",
+      blockHeaderReceiveVote: "1b. Select a Vote",
+      blockHeaderMeeting: "2. Meeting",
+      blockHeaderConformation: "3. Confirmation",
+
+      blockDescGiveVote: "The Vote Giver registers their vote with a location, time, and date on the plattform, making it available.",
+      blockDescReceiveVote: "The Vote Recipient selects one of the listed votes on the plattform, fixing the meeting.",
+      blockDescMeeting: "Both meet at the established location, discuss, and the Vote Giver finally fills out the ballot paper.",
+      blockDescConfirmation: "Both confirm on the platform after meeting up that the vote was exchanged.",
+      blockDescBrowser: "Install the browser extension MetaMask. Write down your seedphrase. Switch to the Ropsten Network. Get test money on https://faucet.metamask.io/ and wait a few minutes.",
+
+      footerContribute: "Do you want to contribute?"
+    },
 });
 
 export default strings;
