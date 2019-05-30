2d4ad369e0f29f0f1c0a7a300bbc853e1053ec0b
Daniel <daniel.e.holler@gmail.com>
2019-05-30 17:11:19 +0200

    french 100%

--- src/components/local.js
+++ src/components/local.js
@@ -83,31 +83,31 @@ let strings = new LocalizedStrings({
       footerContribute: "Vuoi contribuire?"
     },
     fr:{
-        headerIntro: "A platform that enables politically excluded people in Switzerand to vote — Simple to use, social, and meant to persist.",
-        heroTitle: "Including All People in Politics",
-        heroText: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+        headerIntro: "Une plateforme qui permet aux personnes politiquement exclues en Suisse de participer au vote - Simple à utiliser, sociale et destinée à perdurer.",
+        heroTitle: "Intégrer tous les citoyens à la politique",
+        heroText: "En Suisse, de nombreux habitants sont exclus des élections, la plupart d'entre eux étant étrangers et représentant 25 % de la population. Votetandem.org permet une possibilité directe de voter en rencontrant les Suisses qui fournissent leur vote pour l'échange. La plate-forme est basée sur la technologie Blockchain pour un fonctionnement sécurisé et indépendant.",
 
-        roleGiver: "The Vote Giver",
-        roleRecipient: "The Vote Recipient",
+        roleGiver: "Le donneur de vote",
+        roleRecipient: "Le receveur de vote",
 
-        actionSignUp: "Sign Up Here",
+        actionSignUp: "Inscris-toi ici",
 
-        sectionHeaderConcept: "How it works",
-        sectionHeaderBrowser: "Prepare your Browser",
+        sectionHeaderConcept: "Comment cela fonctionne",
+        sectionHeaderBrowser: "Prépare le navigateur",
         sectionHeaderFAQ: "FAQ",
 
-        blockHeaderGiveVote: "1a. Register a Vote",
-        blockHeaderReceiveVote: "1b. Select a Vote",
-        blockHeaderMeeting: "2. Meeting",
+        blockHeaderGiveVote: "1a. Inscris un vote",
+        blockHeaderReceiveVote: "1b. Sélectionne un vote",
+        blockHeaderMeeting: "2. Rendez-vous",
         blockHeaderConformation: "3. Confirmation",
 
-        blockDescGiveVote: "The Vote Giver registers their vote with a location, time, and date on the plattform, making it available.",
-        blockDescReceiveVote: "The Vote Recipient selects one of the listed votes on the plattform, fixing the meeting.",
-        blockDescMeeting: "Both meet at the established location, discuss, and the Vote Giver finally fills out the ballot paper.",
-        blockDescConfirmation: "Both confirm on the platform after meeting up that the vote was exchanged.",
-        blockDescBrowser: "Install the browser extension MetaMask. Write down your seedphrase. Switch to the Ropsten Network. Get test money on https://faucet.metamask.io/ and wait a few minutes.",
+        blockDescGiveVote: "Le donneur de vote inscrit son bulletin de vote et indique le lieu, l'heure et la date sur la plate-forme pour le rendre disponible.",
+        blockDescReceiveVote: "Le receveur de vote cherche un des bulletins de vote listés sur la plate-forme et fixe ainsi la rencontre.",
+        blockDescMeeting: "Tous deux se réunissent à l'endroit établi, discutent et le donneur de vote remplit finalement le bulletin de vote.",
+        blockDescConfirmation: "Tous deux confirment sur la plate-forme après s'être rencontrés que le vote a été échangé.",
+        blockDescBrowser: "Installe l'extension du navigateur MetaMask. Note ton seedphrase. Passe au Ropsten Network. Obtiens l'argent du test sur https://faucet.metamask.io/ et attends quelques minutes.",
 
-        footerContribute: "Do you want to contribute?"
+        footerContribute: "Veux-tu contribuer?"
     },
 });
 
