b6c403048e4a30b051b27292c56fb3eacdda113c
Daniel <daniel.e.holler@gmail.com>
2019-05-30 16:47:35 +0200

    Update local.js

--- src/components/local.js
+++ src/components/local.js
@@ -9,7 +9,7 @@ let strings = new LocalizedStrings({
         roleGiver: "The Vote Giver",
         roleRecipient: "The Vote Recipient",
 
-        actionSignUp: "Give or Get a Vote Here",
+        actionSignUp: "Sign Up Here",
 
         sectionHeaderConcept: "How it works",
         sectionHeaderBrowser: "Prepare your Browser",
@@ -36,7 +36,7 @@ let strings = new LocalizedStrings({
       roleGiver: "Stimmgebende Person",
       roleRecipient: "Stimmsuchende Person",
 
-      actionSignUp: "Gib oder erhalte eine Stimme hier",
+      actionSignUp: "Hier anmelden",
 
       sectionHeaderConcept: "So funktioniert's",
       sectionHeaderBrowser: "Browser vorbereiten",
@@ -51,7 +51,7 @@ let strings = new LocalizedStrings({
       blockDescReceiveVote: "Die stimmsuchende Person wählt eine der aufgeführten Stimmen auf der Plattform aus und fixiert somit das Treffen.",
       blockDescMeeting: "Beide treffen sich am vereinbarten Ort, diskutieren, und die stimmberechtigte Person füllt schließlich den Stimmzettel aus.",
       blockDescConfirmation: "Beide bestätigen nach dem Treffen, auf der Plattform, dass der Stimmzettel ausgetauscht wurde.",
-      blockDescBrowser: "Installiere die Browsererweiterung MetaMask. Schreibe deine Seedphrase auf. Wechsle zum Ropsten-Netzwerk. Hol dir Testgeld auf https://faucet.metamask.io/ und warte ein paar Minuten.",
+      blockDescBrowser: "Installiere die Browsererweiterung MetaMask. Schreibe deine Seedphrase auf. Wechsle zum Ropsten Network. Hol dir Testgeld auf https://faucet.metamask.io/ und warte ein paar Minuten.",
 
       footerContribute: "Willst du deinen Teil beitragen?"
     },
@@ -60,27 +60,54 @@ let strings = new LocalizedStrings({
       heroTitle: "Includendo tutte le persone in politica",
       heroText: "In Svizzera numerosi abitanti sono esclusi dalle elezioni, la maggior parte dei quali sono stranieri, costituendo il 25 % della popolazione. Votetandem.org offre la possibilità di votare direttamente incontrando persone svizzere che forniscono la loro scheda di voto per lo scambio. La piattaforma si basa sulla tecnologia Blockchain per un servizio sicuro e indipendente.",
 
-      roleGiver: "The Vote Giver",
-      roleRecipient: "The Vote Recipient",
+      roleGiver: "Il donatore del voto",
+      roleRecipient: "Il ricevente del voto",
 
-      actionSignUp: "Give or Get a Vote Here",
+      actionSignUp: "Iscriviti qui",
 
-      sectionHeaderConcept: "How it works",
-      sectionHeaderBrowser: "Prepare your Browser",
+      sectionHeaderConcept: "Come funziona",
+      sectionHeaderBrowser: "Prepara il tuo browser",
       sectionHeaderFAQ: "FAQ",
 
-      blockHeaderGiveVote: "1a. Register a Vote",
-      blockHeaderReceiveVote: "1b. Select a Vote",
-      blockHeaderMeeting: "2. Meeting",
-      blockHeaderConformation: "3. Confirmation",
+      blockHeaderGiveVote: "1a. Registra un voto",
+      blockHeaderReceiveVote: "1b. Seleziona un voto",
+      blockHeaderMeeting: "2. Incontro",
+      blockHeaderConformation: "3. Conferma",
 
-      blockDescGiveVote: "The Vote Giver registers their vote with a location, time, and date on the plattform, making it available.",
-      blockDescReceiveVote: "The Vote Recipient selects one of the listed votes on the plattform, fixing the meeting.",
-      blockDescMeeting: "Both meet at the established location, discuss, and the Vote Giver finally fills out the ballot paper.",
-      blockDescConfirmation: "Both confirm on the platform after meeting up that the vote was exchanged.",
-      blockDescBrowser: "Install the browser extension MetaMask. Write down your seedphrase. Switch to the Ropsten Network. Get test money on https://faucet.metamask.io/ and wait a few minutes.",
+      blockDescGiveVote: "Il donatore del voto registra la sua scheda elettorale e indica luogo, ora e data sulla piattaforma mettendola a disposizione.",
+      blockDescReceiveVote: "Il ricevente del voto seleziona uno dei voti elencati sulla piattaforma, fissando l'incontro.",
+      blockDescMeeting: "Entrambi s'incontrano nel luogo stabilito, discutono e il donatore del voto alla fine compila la scheda elettorale.",
+      blockDescConfirmation: "Entrambi confermano sulla piattaforma, dopo l'incontro avvenuto, che la scheda di voto è stata scambiata.",
+      blockDescBrowser: "IInstalla l'estensione del browser MetaMask. Annota la seedphrase. Passa al Ropsten Network. Ricevi denaro da test su https://faucet.metamask.io/ e aspetta qualche minuto.",
 
-      footerContribute: "Do you want to contribute?"
+      footerContribute: "Vuoi contribuire?"
+    },
+    fr:{
+        headerIntro: "A platform that enables politically excluded people in Switzerand to vote — Simple to use, social, and meant to persist.",
+        heroTitle: "Including All People in Politics",
+        heroText: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+
+        roleGiver: "The Vote Giver",
+        roleRecipient: "The Vote Recipient",
+
+        actionSignUp: "Sign Up Here",
+
+        sectionHeaderConcept: "How it works",
+        sectionHeaderBrowser: "Prepare your Browser",
+        sectionHeaderFAQ: "FAQ",
+
+        blockHeaderGiveVote: "1a. Register a Vote",
+        blockHeaderReceiveVote: "1b. Select a Vote",
+        blockHeaderMeeting: "2. Meeting",
+        blockHeaderConformation: "3. Confirmation",
+
+        blockDescGiveVote: "The Vote Giver registers their vote with a location, time, and date on the plattform, making it available.",
+        blockDescReceiveVote: "The Vote Recipient selects one of the listed votes on the plattform, fixing the meeting.",
+        blockDescMeeting: "Both meet at the established location, discuss, and the Vote Giver finally fills out the ballot paper.",
+        blockDescConfirmation: "Both confirm on the platform after meeting up that the vote was exchanged.",
+        blockDescBrowser: "Install the browser extension MetaMask. Write down your seedphrase. Switch to the Ropsten Network. Get test money on https://faucet.metamask.io/ and wait a few minutes.",
+
+        footerContribute: "Do you want to contribute?"
     },
 });
 
