42f262a674779cd0e6acd0986b5c9ab2fa8f8f44
znezniV <vleutenegger@gmail.com>
2019-05-30 15:12:02 +0200

    Localized most strings

--- src/components/local.js
+++ src/components/local.js
@@ -3,6 +3,29 @@ import LocalizedStrings from 'react-localization';
 let strings = new LocalizedStrings({
     en:{
         cottageCheese:"Cottage Cheese",
+        headerIntro: "A platform that enables politically excluded people in Switzerand to vote — Simple to use, social, and meant to persist.",
+        heroTitle: "Including All People in Politics",
+        heroText: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+        
+        roleGiver: "The Vote Giver",
+        roleRecipient: "The Vote Recipient",
+        
+        sectionHeaderConcept: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+        sectionHeaderBrowser: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+        sectionHeaderFAQ: "In Switzerland many inhabitants are excluded from votings, most of whom are foreigners making up 25 % of the population. Votetandem.org enables a direct possibility to vote by meeting up with Swiss people who supply their vote for exchange. The platform is based on blockchain technology for a secure and independent operation.",
+
+        boxHeaderGiveVote: "1a. Register a Vote",
+        boxHeaderReceiveVote: "1b. Select a Vote",
+        boxHeaderMeeting: "2. Meeting",
+        boxHeaderConformation: "3. Confirmation",
+
+        boxDescGiveVote: "The Vote Giver registers their vote with a location, time, and date on the plattform, making it available.",
+        boxDescReceiveVote: "The Vote Recipient selects one of the listed votes on the plattform, fixing the meeting.",
+        boxDescMeeting: "Both meet at the established location, discuss, and the Vote Giver finally fills out the ballot paper.",
+        boxDescConfirmation: "Both confirm on the platform after meeting up that the vote was exchanged.",
+        boxDescBrowser: "Install the browser extension MetaMask. Write down your seedphrase. Switch to the Ropsten Network. Get test money on https://faucet.metamask.io/ and wait a few minutes.",
+
+        footerContribute: "Do you want to contribute?"
     },
     de: {
         cottageCheese:"Hüttechäs",
