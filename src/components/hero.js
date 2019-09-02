61b14e22845ae72dab97d12ebf938c4d0715e6f0
znezniV <vleutenegger@gmail.com>
2019-09-02 16:13:47 +0200

    add new logo

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,12 @@
 import React from "react"
 import strings from "../components/local"
-import Cross from "../images/SquareSwiss.svg";
+import VotetandemLogo from "../images/votetandem-logo.svg";
 
 const Hero = () => (
     <div className="hero">
         <h2 className="hero__title">{strings.heroTitle}</h2>
         <p className="hero__text">
-            {/* <Cross /> */}
-            <img className="hero__text-image" src={Cross} alt="Square and cheesy Swiss cross."/>
+            <img className="hero__text-image" src={VotetandemLogo} alt="votetandem.org logo"/>
             {strings.heroText}
         </p>
     </div>
